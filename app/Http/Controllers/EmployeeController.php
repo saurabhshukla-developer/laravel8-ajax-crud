26bfcd60ff91981b1d468c6ab0ee547c86d88c45
Saurabh Shukla <saurabhshukla.developer@gmail.com>
2021-10-10 00:10:56 +0530

    employee crud using ajax done

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+use App\Http\Resources\EmployeeResource;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employees = Employee::get();
+        return EmployeeResource::collection($employees);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $employee = new Employee();
+        $employee->name = $request->name;
+        $employee->email = $request->email;
+        $employee->mobile = $request->mobile;
+        $employee->designation = $request->designation;
+        $employee->salary = $request->salary;
+        if($employee->save()) {
+            return new EmployeeResource($employee);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $employee = Employee::findOrFail($id);
+        return new EmployeeResource($employee);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $employee = Employee::findOrFail($id);
+        $employee->name = $request->name;
+        $employee->email = $request->email;
+        $employee->mobile = $request->mobile;
+        $employee->designation = $request->designation;
+        $employee->salary = $request->salary;
+        if($employee->save()) {
+            return new EmployeeResource($employee);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $employee = Employee::findOrFail($id);
+        if($employee->delete()) {
+            return new EmployeeResource($employee);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/employees', [EmployeeController::class, 'index']);
+Route::post('/employee', [EmployeeController::class, 'store']);
+Route::get('/employee/{id}', [EmployeeController::class, 'show']);
+Route::post('/update/employee/{id}', [EmployeeController::class, 'update']);
+Route::get('/delete/employee/{id}', [EmployeeController::class, 'destroy']);
